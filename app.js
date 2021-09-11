a1f36ec974d4fd5b052af7735a74754ef1cd519e
0xdeee <21086243+0xdeee@users.noreply.github.com>
2021-09-11 09:00:19 +0530

    added error handling to routes

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const morgan = require('morgan');
 const ejsMate = require('ejs-mate');
 
 const Campground = require('./models/campground');
+const catchAsync = require('./helpers/catchAsync');
+const ExpressError = require('./helpers/ExpressError');
+const { validateCampgroundSchema } = require('./schema');
 
 mongoose
   .connect('mongodb://localhost:27017/yelp-camp', {
@@ -25,51 +28,93 @@ app.use(methodOverride('_method'));
 app.use(morgan('common'));
 app.engine('ejs', ejsMate);
 
-// starting the express server
-const port = 8080;
-app.listen(port, () => {
-  console.log('server started and listening to 8080');
-});
+const validateCampground = (req, res, next) => {
+  const { error } = validateCampgroundSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(', ');
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
 
 app.get('/', (req, res) => {
-  res.send('home');
+  res.render('home');
 });
 
-app.get('/campgrounds', async (req, res) => {
-  const campgrounds = await Campground.find({});
-  res.render('campgrounds/index', { campgrounds });
-});
+app.get(
+  '/campgrounds',
+  catchAsync(async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds });
+  })
+);
 
 app.get('/campgrounds/new', (req, res) => {
   res.render('campgrounds/new');
 });
 
-app.get('/campgrounds/:id', async (req, res) => {
-  const { id } = req.params;
-  const campground = await Campground.findById(id);
-  res.render('campgrounds/show', { campground });
+app.get(
+  '/campgrounds/:id',
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    res.render('campgrounds/show', { campground });
+  })
+);
+
+app.post(
+  '/campgrounds',
+  validateCampground,
+  catchAsync(async (req, res, next) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+  })
+);
+
+app.get(
+  '/campgrounds/:id/edit',
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    res.render('campgrounds/edit', { campground });
+  })
+);
+
+app.patch(
+  '/campgrounds/:id',
+  validateCampground,
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    res.redirect(`/campgrounds/${id}`);
+  })
+);
+
+app.delete(
+  '/campgrounds/:id/delete',
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+  })
+);
+
+// error handlers
+
+app.all('*', (req, res, next) => {
+  next(new ExpressError('Page not found', 404));
 });
 
-app.post('/campgrounds', async (req, res) => {
-  const campground = new Campground(req.body);
-  await campground.save();
-  res.redirect('/campgrounds');
+app.use(function (err, req, res, next) {
+  const { statusCode = 500, message = 'Something went wrong' } = err;
+  if (!err.message) err.message = 'Something went wrong';
+  res.status(statusCode).render('error', { err });
 });
 
-app.get('/campgrounds/:id/edit', async (req, res) => {
-  const { id } = req.params;
-  const campground = await Campground.findById(id);
-  res.render('campgrounds/edit', { campground });
-});
-
-app.patch('/campgrounds/:id', async (req, res) => {
-  const { id } = req.params;
-  await Campground.findByIdAndUpdate(id, req.body);
-  res.redirect(`/campgrounds/${id}`);
-});
-
-app.delete('/campgrounds/:id/delete', async (req, res) => {
-  const { id } = req.params;
-  await Campground.findByIdAndDelete(id);
-  res.redirect('/campgrounds');
+// starting the express server
+const port = 8080;
+app.listen(port, () => {
+  console.log('server started and listening to 8080');
 });
