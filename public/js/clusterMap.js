084be5f73c74ba1e6f2885e1c54855f7784de8a2
0xdeee <21086243+0xdeee@users.noreply.github.com>
2021-12-12 07:42:01 +0530

    clusterMap improvements

--- public/js/clusterMap.js
+++ public/js/clusterMap.js
@@ -2,7 +2,7 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/dark-v10',
-  center: [-103.5917, 40.6699],
+  center: [clusterMapCenterLong, clusterMapCenterLat],
   zoom: 3,
 });
 
